0b63ff0582fd3ae4ed3efab48979c95a98736ad2
vishnu <vishnu04715@gmail.com>
2024-09-05 10:14:52 -0700

    Home Page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,28 @@
 import React from "react";
 import { Row, Col } from "react-bootstrap";
+import { Link } from "react-router-dom";
 function Home() {
   return (
     <>
       <div className="container-fluid bg-success p-4 mb-4 " style={{ width: "100%", height: "100vh" }}>
         <Row>
+          <Col md={6} className="d-flex justify-center align-items-center flex-column">
+            <div>
+              <h3 className="text-light">Project Fair</h3>
+              <h6>One stop destination for may software project</h6>
+            </div>
+          </Col>
+          <Link >
+          <button className="btn btn-outline my-4">
+            GET STARTED
+          </button>
+          </Link>
           <Col></Col>
-          <Col></Col>
-          <C
+          
         </Row>
-        <Row
+        
       </div>
+     
     </>
   );
 }
