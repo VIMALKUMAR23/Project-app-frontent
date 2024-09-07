5ecb1a637f2b7f1d3504ed312f6da16859c645b3
vishnu <vishnu04715@gmail.com>
2024-09-06 20:55:33 -0700

    page&components -fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+
 function Footer() {
   return (
     <>
-        <div className='d-flex justify-content-center align-items-center  bg-success p-3'>
+    
+    <div className='d-flex justify-content-center align-items-center bg-success p-3'>
       <div className='footer d-flex align-items-center justify-content-evenly'>
         <div style={{ width: '400px' }}>
           <h5 className='textStyle'><i class="fa-brands fa-stack-overflow"></i> Project Fair</h5>
@@ -23,13 +25,13 @@ function Footer() {
         </div>
         <div className='d-flex flex-column ms-5'>
           <h4 className='textStyle'>Guides</h4>
-          <Link to='https://react.dev/' target='_blank' style={{ textDecoration: 'none', color: 'white' }}>
+          <Link to='https://react.dev/' target='_blank' style={{ textDecoration: 'none', color: 'black' }}>
             React
           </Link>
-          <Link to='https://react-bootstrap.github.io/' target="_blank" style={{ textDecoration: 'none', color: 'white' }}>
+          <Link to='https://react-bootstrap.github.io/' target="_blank" style={{ textDecoration: 'none', color: 'black' }}>
             React Bootsrap
           </Link>
-          <Link to='https://www.npmjs.com/package/json-server' target="_blank" style={{ textDecoration: 'none', color: 'white' }}>
+          <Link to='https://www.npmjs.com/package/json-server' target="_blank" style={{ textDecoration: 'none', color: 'black' }}>
             Json Server
           </Link>
         </div>
@@ -40,16 +42,16 @@ function Footer() {
             <button className='btn btn-warning ms-2'>SUBSCRIBE</button>
           </div>
           <div className='d-flex justify-content-evenly align-items-center mt-3' >
-            <Link style={{ textDecoration: 'none', color: 'white' }}>
+            <Link style={{ textDecoration: 'none', color: 'black' }}>
               <i class="fa-brands fa-instagram fa-2x"></i>
             </Link>
-            <Link style={{ textDecoration: 'none', color: 'white' }}>
+            <Link style={{ textDecoration: 'none', color: 'black' }}>
               <i class="fa-brands fa-twitter fa-2x"></i>
             </Link>
-            <Link style={{ textDecoration: 'none', color: 'white' }}>
+            <Link style={{ textDecoration: 'none', color: 'black' }}>
               <i class="fa-brands fa-facebook fa-2x"></i>
             </Link>
-            <Link style={{ textDecoration: 'none', color: 'white' }}>
+            <Link style={{ textDecoration: 'none', color: 'black' }}>
               <i class="fa-brands fa-reddit fa-2x"></i>
             </Link>
           </div>
@@ -58,8 +60,7 @@ function Footer() {
         </div>
       </div>
 
-    </div>
-    </>
+    </div></>
   )
 }
 
